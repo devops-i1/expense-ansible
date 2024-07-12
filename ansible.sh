e2aaf551231f88a2cc860a8d6618f3aa7f39deda
Akhil <akhilmahendra035@gmail.com>
2024-07-12 13:56:35 +0530

    Testing env vars

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,4 @@
 component=$1
-ansible-playbook -i $component-dev.akhilsaidevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
+env
+
+ansible-playbook -i $component-$env.akhilsaidevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
