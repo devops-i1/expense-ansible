7f6c9deb517d63f6719b200d32ef4dbf8b90b722
Akhil <akhilmahendra035@gmail.com>
2024-09-14 13:36:47 +0530

    Testing secure variables

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,3 @@
 component=$1
-env >/tmp/env
 
-#ansible-playbook -i $component-$env.akhilsaidevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
+ansible-playbook -i $component-$env.akhilsaidevops.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
