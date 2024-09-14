925f3d6164b707b7b41280fd6192b373053ea045
Akhil <akhilmahendra035@gmail.com>
2024-09-14 19:04:02 +0530

    update secrets in terraform

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
 component=$1
 
 ansible-playbook get-secrets.yml -e vault_token=$vault_token
-#ansible-playbook -i $component-$env.akhilsaidevops.online, -e env=$env -e role_name=$component expense.yml
+ansible-playbook -i $component-$env.akhilsaidevops.online, -e env=$env -e role_name=$component expense.yml -e '@secrets.json'
